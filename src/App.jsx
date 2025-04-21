faccf295ed329672b2211ed8a3aac158d7d6d00e
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-04-21 23:26:20 +0700

    feat: add deployment file and fix content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import './App.css'
+import {data} from './constants'
 
 
 function App() {
-
+  const [currentData, setCurrentData] = useState(0)
   return (
     <div className="App">
+      <img src='/mcbn_alternate_logo.png' width={150}/>
+      <div className='text-container'>
+          <h1>{data[currentData].username}</h1>
+          <p>{data[currentData].text}</p>
+          <p className='pages'>{currentData} / {data.length - 1}</p>
+      </div>
+      <div className='btn-container'>
+        <button className='btn' disabled={currentData === 0} onClick={() => setCurrentData(currentData - 1)}>Prev</button>
+        <button className='btn' disabled={currentData === data.length - 1} onClick={() => setCurrentData(currentData + 1)}>Next</button>
+      </div>
     </div>
   )
 }
---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        username: "test 1",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus quia sed, libero enim temporibus sit facilis dignissimos maiores nulla nihil hic quidem dolor dolore repudiandae ipsum eaque sapiente voluptatem. Sequi laborum voluptatibus rerum omnis totam nemo fugiat ut voluptas reiciendis eveniet, facilis minus quas molestias nihil, quidem eos temporibus molestiae 1?"
+    },
+    {
+        username: "test 2",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus quia sed, libero enim temporibus sit facilis dignissimos maiores nulla nihil hic quidem dolor dolore repudiandae ipsum eaque sapiente voluptatem. Sequi laborum voluptatibus rerum omnis totam nemo fugiat ut voluptas reiciendis eveniet, facilis minus quas molestias nihil, quidem eos temporibus molestiae 2?"
+    },
+    {
+        username: "test 3",
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus quia sed, libero enim temporibus sit facilis dignissimos maiores nulla nihil hic quidem dolor dolore repudiandae ipsum eaque sapiente voluptatem. Sequi laborum voluptatibus rerum omnis totam nemo fugiat ut voluptas reiciendis eveniet, facilis minus quas molestias nihil, quidem eos temporibus molestiae?"
+    },
+]
